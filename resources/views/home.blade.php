0bbea22aab36788e0edefd81e3f695a106c463bc
José Alejandro Valduz Contreras <jose.valduz@unet.edu.ve>
2023-08-14 20:24:13 -0400

    reparacion de layout app blade creacion basica de vistas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,40 +1,71 @@
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+@extends('layouts.app')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@section('title', 'Home')
 
-                    {{ __('You are logged in!') }}
+@section('content')
+    <h1>Bienvenido a HOME</h1>
+@endsection
+
+{{-- <!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    {{-- favicon --
+    {{-- estilos --
+</head>
+
+<body>
+    {{-- header --}}
+{{-- nav --}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="card-body">
-    <h4>Bienvenido . {{ auth()->user()->name }} </h4>
-    <h4>Ubicacion . {{ auth()->user()->ubicacion }} </h4>
-</div>
+    <div class="card-body">
+        <h4>Bienvenido . {{ auth()->user()->name }} </h4>
+        <h4>Ubicacion . {{ auth()->user()->ubicacion }} </h4>
+    </div>
 
-{{-- logout --}}
-<div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-    <a class="dropdown-item" href="{{ route('logout') }}"
-        onclick="event.preventDefault();
-                     document.getElementById('logout-form').submit();">
-        {{ __('Logout') }}
-    </a>
 
-    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-        @csrf
-    </form>
-</div>
+    {{-- footer --}}
+{{-- logout --}
+    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
+    </div>
+ --}}
+{{-- script --}
+</body>
+
+</html>
+
 {{-- @extends('layouts.app')
 
 @section('content')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,64 @@
-<!doctype html>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    {{-- favicon --}}
+    {{-- estilos --}}
+</head>
+
+<body>
+    {{-- header --}}
+    {{-- nav --}}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        {{ __('You are logged in!') }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @yield('content')
+
+    <div class="card-body">
+        <h4>Bienvenido . {{ auth()->user()->name }} </h4>
+        <h4>Ubicacion . {{ auth()->user()->ubicacion }} </h4>
+    </div>
+
+    {{-- footer --}}
+    {{-- logout --}}
+    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
+    </div>
+
+    {{-- script --}}
+</body>
+
+</html>
+
+{{-- <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
@@ -83,4 +143,4 @@
     </div>
 </body>
 
-</html>
+</html> --}}
