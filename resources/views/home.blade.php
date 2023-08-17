e92963b9d99b6055c2d1ae37e7a1ba9fe96d7ef5
José Alejandro Valduz Contreras <jose.valduz@unet.edu.ve>
2023-08-16 21:19:21 -0400

    Arreglando Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,20 +4,34 @@
 
 @section('content')
 
-    <h1>Bienvenido a HOME</h1>
 
-    <p>Platos servidos: </p>
+    <h1 class="text-center">Platos servidos: +1.000.000</h1>
 
     <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="..." class="d-block w-100" alt="...">
+                <img src="{{ asset('img/f1.jpg') }}" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
+                <img src="{{ asset('img/f2.jpg') }}" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
+                <img src="{{ asset('img/f3.jpg') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('img/f4.jpg') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('img/f5.jpg') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('img/f6.jpg') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('img/f7.jpg') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('img/f8.jpg') }}" class="d-block w-100" alt="...">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
@@ -31,6 +45,41 @@
             <span class="visually-hidden">Next</span>
         </button>
     </div>
+
+    <div class="container mt-5">
+        <h1 class="text-center mb-4">Comedores Solidarios del Municipio Junín</h1>
+
+        <div class="row">
+            <div class="col-lg-6 text-center">
+                <h2 class="text-primary ">AC AMIJUNÍN</h2>
+                <p>Número de RIF: J-411084387</p>
+                <div class="container-sm">
+                    <img class="container-sm" src="{{ asset('img/LOGO1A.png') }}" alt="">
+                </div>
+                <!-- ... Agrega más detalles aquí ... -->
+            </div>
+
+            <div class="col-lg-6">
+                <h2 class="text-primary">COMEDORES SOLIDARIOS DE JUNÍN</h2>
+                <p>Objetivo: Proporcionar apoyo alimentario, educativo, médico y psicológico a niños en edad escolar (2 – 12
+                    años) de los sectores más vulnerables de la ciudad de Rubio, municipio Junín.</p>
+                <!-- ... Agrega más detalles aquí ... -->
+                <div class="container-sm bg-light">
+                    <img class="container-sm" src="{{ asset('img/Logo Alimenta.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <!-- Agrega más información detallada sobre los comedores aquí... -->
+        </div>
+
+        <h2 class="mt-5 text-primary">Información de Contacto</h2>
+        <p>Dr. Francisco José Gamboa Valderrama</p>
+        <p>Coordinador General</p>
+        <!-- ... Agrega más detalles de contacto aquí ... -->
+    </div>
+
 @endsection
 
 {{-- <!DOCTYPE html>
